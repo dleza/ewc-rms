65028aeae46fa3e2aea7cc9244dbffbbef81d262
dleza <lezadanny@gmail.com>
2026-03-07 01:59:38 +0200

    Make user profile creation safe

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -18,17 +20,8 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
-    
-    from django.db.models.signals import post_save
-    from django.dispatch import receiver
 
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            UserProfile.objects.create(user=instance)
-
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.userprofile.save()
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    UserProfile.objects.get_or_create(user=instance)
